5b8d54b37907e1b499a80943c18d7ff3eb637415
joshuashevchuk <joshua.shevchuk@ookla.com>
2024-12-18 00:22:25 -0800

    add db.go

--- mongo/db.go
+++ mongo/db.go
@@ -2,6 +2,8 @@
 package main
 
 import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
 )
@@ -14,5 +16,13 @@ func main() {
 	// Use the SetServerAPIOptions() method to set the Stable API version to 1
 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
 	log.Println("Connecting to MongoDB", serverAPI)
+
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+	// Create a new client and connect to the server
+	client, err := mongo.Connect(context.TODO(), opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("client is ", client)
 
 }
